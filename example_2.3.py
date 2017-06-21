7af6e44ea6d6821448a29f3435b73c3102127d81
themelok <3038686@gmail.com>
2017-06-21 22:12:20 +0300

    example_2.3 - ok

--- example_2.3.py
+++ example_2.3.py
@@ -21,7 +21,7 @@ class ContinuousGeneticAlgorithm:
     Stop condition: 100 generations
     """
 
-    def __init__(self, func, mut_prob=0.3, pop_range=(), pop_size=12):
+    def __init__(self, func, mut_prob=0.2, pop_range=(), pop_size=12):
         self.func = func                        # Fitness function
         self.population = []                    # Population
         self.mutation_probability = mut_prob    # Mutation probability, 0.3 by default
@@ -29,6 +29,9 @@ class ContinuousGeneticAlgorithm:
         self.npar = 2                           # Number of optimization variables
         self.population_size = pop_size         # Population size, 12 by default
         self.max_generations =100               # Count of generations
+        self.keep = self.population_size//2     # Count of parents
+        self.prob = np.flipud(np.arange(1, self.keep + 1) / np.sum(np.arange(1, self.keep + 1)))
+        self.odds = np.insert(np.cumsum(self.prob), 0, 0)
 
     def generate_start_population(self):
         """Size of population equal population_size.
@@ -39,32 +42,62 @@ class ContinuousGeneticAlgorithm:
     def selection(self, population):
         """Selecting of population's members by rank, which will be a parents wis 'prob' probability"""
         res = np.argsort([self.func(*item) for item in population])
-        new_pop = np
-        new_pop = np.delete(population, res[len(res)//2:], 0)
+        res = res[:self.keep]
+        new_pop = np.array(self.population)[res]
         return new_pop
+
+    def mating(self, parents):
+        """Random selecting of pairs within parents.
+        ma and pa contain the indices of the chromosomes that will mate
+        """
+        offsprings = []
+        M = (self.population_size - self.keep) // 2
+        pick1 = np.random.rand(M)
+        pick2 = np.random.rand(M)
+        ma = []
+        pa = []
+        for ic in np.arange(M):
+            for id in np.arange(1, self.keep + 1):
+                if pick1[ic] <= self.odds[id] and pick1[ic] > self.odds[id - 1]:
+                    ma.append(id - 1)
+                if pick2[ic] <= self.odds[id] and pick2[ic] > self.odds[id - 1]:
+                    pa.append(id - 1)
+        for i in range(M):
+            """Here is crossover. crosover_point is number of gen in i's chromosome(parent)"""
+            b=np.random.rand()
+            crossover_point = np.random.randint(0,self.npar)
+            mam = copy.copy(parents[ma[i]])
+            pap = copy.copy(parents[pa[i]])
+            mam_xy = mam[crossover_point]
+            pap_xy = pap[crossover_point]
+            mam[crossover_point]=(1-b)*mam_xy+b*pap_xy
+            offsprings.append(mam.tolist())
+            pap[crossover_point]=(1-b)*pap_xy+b*mam_xy
+            offsprings.append(pap.tolist())
+        return offsprings
+    def mutation(self, chromosome):
+        """Implements mutation of chromosomes with probability self.mutation_probability"""
+        r = np.random.rand()
+        if r <=self.mutation_probability:
+            mutation_gen = np.random.randint(0,self.npar)
+            chromosome[mutation_gen]= (self.population_range[1] - self.population_range[0])*np.random.rand()\
+                                      +self.population_range[0]
+        return chromosome
 
     def evolute(self):
         """Main method of evolution finding"""
         self.generate_start_population()
-        keep = self.population_size//2
-        prob = np.flipud(np.arange(1, keep + 1) / np.sum(np.arange(1, keep + 1)))
-        odds = np.insert(np.cumsum(prob), 0, 0)
         print(print(self.population))
         for n in range(self.max_generations):
             population = copy.copy(self.population)
-            new_pop = self.selection(population)
-            M = (self.population_size - keep) // 2
-            pick1 = np.random.rand(M)
-            pick2 = np.random.rand(M)
-            ma = []
-            pa = []
-            for ic in np.arange(M):
-                for id in np.arange(1, keep + 1):
-                    if pick1[ic] <= odds[id] and pick1[ic] > odds[id - 1]:
-                        ma.append(id - 1)
-                    if pick2[ic] <= odds[id] and pick2[ic] > odds[id - 1]:
-                        pa.append(id - 1)
-            print(ma, pa)
+            parents = self.selection(population)
+            offsprings = self.mating(parents)
+            new_populaion=np.concatenate((parents,offsprings))
+            mutated_new_population = np.array([self.mutation(chromosome) for chromosome in new_populaion])
+            print("new pop: ",mutated_new_population)
+            self.population=mutated_new_population
+
+
 
 
 
